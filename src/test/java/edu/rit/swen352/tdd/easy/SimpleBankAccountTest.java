8e0af29afad91e90da6c89ceef2b60fb9c878735
conlan <cac9809@gmail.com>
2026-06-24 08:39:00 -0400

    ctor : Bank Account with initial balance : create failing test

--- src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
@@ -1,13 +1,20 @@
 package edu.rit.swen352.tdd.easy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test suite for the {@link SimpleBankAccount} component.
  */
 class SimpleBankAccountTest {
+
+    @Test
+    @DisplayName("ctor with an initial balance")
+        void testConstructorWithInitislBalnce() {
+        SimpleBankAccount account = new SimpleBankAccount(100.0f);
+
+        assertEquals(100.0f, account.getBalance());
+    }
 
 }
