7db131f44792227853a38527c38d2616f8cbaa85
Richard Tang <rt6412@rit.edu>
2026-06-24 20:14:37 -0400

    of_1: test for of returning null: create passing code

--- src/main/java/edu/rit/swen352/tdd/easy/MyOptional.java
+++ src/main/java/edu/rit/swen352/tdd/easy/MyOptional.java
@@ -44,8 +44,11 @@ public class MyOptional<T> {
         this.value = value;
     }
 
-    public static <T> void of(T value) {
-        assert false: "Not yet implemented";
+    public static <T> MyOptional<T> of(T value) {
+        if(value == null) {
+            throw new NullPointerException("Value cannot be false");
+        }
+        return new MyOptional<T>(value);
     }
 
 
