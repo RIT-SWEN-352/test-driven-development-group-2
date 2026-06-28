8f7de069ffc198aaaef54bae3c7b5881e3e0c46c
Richard Tang <rt6412@rit.edu>
2026-06-28 09:12:01 -0400

    size_1: size is 0 for empty stack

--- src/main/java/edu/rit/swen352/tdd/hard/MyStack.java
+++ src/main/java/edu/rit/swen352/tdd/hard/MyStack.java
@@ -67,8 +67,8 @@ public class MyStack<T> {
         return size == 0;
     }
 
-    public void size() {
-        assert false: "Not yet implemented";
+    public int size() {
+        return size;
     }
 
 }
